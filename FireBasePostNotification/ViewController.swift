30bea1c20ddb8b69f2d1100dbb3a3333df4312b1
Rajat Bhatt <rajatbhatt@Rajats-Mac-mini.local>
2017-02-16 11:42:49 +0530

    Initial Commit

---
+++ FireBasePostNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBasePostNotification
+//
+//  Created by Rajat Bhatt on 2/16/17.
+//  Copyright © 2017 Rajat Bhatttixdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
